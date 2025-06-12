b1b672486d0835d451e3c00ba024ff30cc5e8582
ZizahSwiftie <azizahulnha@gmail.com>
2025-06-12 10:42:47 +0700

    Fix display() function logic in circular queue implementation

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -42,4 +42,65 @@ public:
         }
         queue_array[FEAR] = num; 
     }
+    void remove()
+    {
+        // Cek apakah antrian kosong
+        if (FRONT == -1) 
+        {
+            cout << "Queue underflow\n"; 
+            return; 
+        }
+        cout << "\nElement deleted from the queue is: " << queue_array[FRONT] << "\n";
 
+        // Cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR) 
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            // JIka elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1) 
+                FRONT = 0; 
+            else 
+                FRONT = FRONT + 1;
+        } 
+    }
+
+    void display()
+    {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        // Cek apakah antrian kosong
+        if (FRONT == -1) 
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "Queue is empty\n";
+        return;
+    }
+
+    cout << "\nElements in the queue are...\n";
+
+    // Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+    if (FRONT_position <= REAR_position)
+    {
+        cout << queue_array[FRONT_position] << " ";
+        FRONT_position++;
+    }
+    cout << endl;
+}
+else
+{
+    // Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array 
+    while (FRONT_position <= max - 1) 
+    {
+        cout << queue_array[FRONT_position] << " ";
+        FRONT_position++;
+    }
+    FRONT_position = 0; 
+}
